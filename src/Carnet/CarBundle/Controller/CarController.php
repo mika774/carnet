46c8b897b417c7eaa4d8fe3a7e6e121daaca8e8c
Mickael ANDREO <Mika@macbook-pro-de-mickael.home>
2016-10-24 17:58:31 +0200

    Update CarController

---
+++ src/Carnet/CarBundle/Controller/CarController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Carnet\CarBundle\Controller;
+
+use Carnet\CarBundle\Entity\Car;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CarController extends Controller
+{
+    public function indexAction($page)
+    {
+    	if ($page < 1) {
+    		throw new NotFoundHttpException("La page ".$id." n'existe pas.");
+    	}
+
+    	$em = $this->getDoctrine()->getManager();
+
+    	$listCars = $em->getRepository('CarnetCarBundle:Car')->findAll();
+
+
+        return $this->render('CarnetCarBundle:Car:index.html.twig', array(
+        	'listCars' => $listCars
+        ));
+    }
+
+    public function viewAction($id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+
+    	$car = $em->getRepository('CarnetCarBundle')->find($id);
+
+    	if (null === $car) {
+    		throw new NotFoundHttpException("La voiture d'id ".$id." n'existe pas");
+    	}
+
+    	return $this->render('CarnetCarBundle:Car:view.html.twig', array(
+    		'car' => $car
+    	));
+    }
+
+    public function addAction(Request $request)
+    {
+    	$em = $this->getDoctrine()->getManager();
+
+    	if ($request->isMethod('POST')) {
+    		$request->getSession()->getFlashBag()->add('notice', 'Voiture bien enregistrée');
+
+    		return $this->redirectToRoute('carnet_car_view', array('id' => $car->getId()));
+    	}
+
+    	return $this->render('CarnetCarBundle:Car:add.html.twig');
+    }
+
+    public function updateAction($id, Request $request)
+    {
+    	$em = $this->getDoctrine()->getManager();
+
+    	$car = $em->getRepository('CarnetCarBundle:Car')->find($id);
+
+    	if (null === $car) {
+    		throw new NotFoundHttpException("La voiture d'id ".$id." n'existe pas.");
+    	}
+
+    	if ($request->isMethod('POST')) {
+    		$request->getSession()->getFlashBag()->add('notice', 'Voiture bien modifiée');
+
+    		return $this->redirectToRoute('carnet_car_view', array('id' => $car->getId()));
+    	}
+
+    	return $this->render('CarnetCarBundle:Car:update.html.twig');
+    }
+
+    public function deleteAction($id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+
+    	$car = $em->getRepository('CarnetCarBundle:Car')->find($id);
+
+    	if (null === $car) {
+    		throw new NotFoundHttpException("La voiture d'id ".$id." n'existe pas.");
+    	}
+
+    	return $this->render('CarnetCarBundle:Car:delete.html.twig');
+    }
+}
