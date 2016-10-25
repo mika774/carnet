7a881e9f8bfe0b1b3097cc59caa926bb05c062e6
Mickael ANDREO <Mika@macbook-pro-de-mickael.home>
2016-10-25 18:16:44 +0200

    Add form for Car

---
+++ src/Carnet/CarBundle/Form/CarType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Carnet\CarBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CarType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mark',   TextType::class)
+            ->add('model',  TextType::class)
+            ->add('note',   TextareaType::class)
+            ->add('save',   SubmitType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Carnet\CarBundle\Entity\Car'
+        ));
+    }
+}
---
+++ src/Carnet/CarBundle/Form/CarUpdateType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Carnet\CarBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CarUpdateType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        
+    }
+    
+    public function getParent()
+    {
+        return CarType::class;
+    }
+}
