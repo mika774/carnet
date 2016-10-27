9f687e5a46ec53e2762c92d2c578ce61fbbeefe3
Mickael ANDREO <Mika@macbook-pro-de-mickael.home>
2016-10-27 10:27:48 +0200

    Add Reparation Entity

---
+++ src/Carnet/CarBundle/Entity/Reparation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Carnet\CarBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reparation
+ *
+ * @ORM\Table(name="reparation")
+ * @ORM\Entity(repositoryClass="Carnet\CarBundle\Repository\ReparationRepository")
+ */
+class Reparation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Reparation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
